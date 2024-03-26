34ab8192f37ae3020d08e171f390403b5f8f14c2
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-03-26 16:43:42 +0200

    Added at least 5 songs

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const songs = [
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
     // Feel free to add even more songs
+    { title: "Boheimian Rhapsody", artist: "Queen", genre: "Rock"},
+    { title: "Billie", artist: "Micheal Jackson", genre: "Pop"},
+    { title: "Purple Rain", artist: "Price", genre: "Pop"},
+    { title: "Stairway to Heaven", artist:"Led Zeppelin", genre:"Rock"},
+    { title: "Respect", artist: "Aretha Franklin", genre: "R&B"}
 ];
 
 
